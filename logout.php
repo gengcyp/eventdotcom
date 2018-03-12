7bd61e121344c3b1664cf389b59d63c070c75278
5710404306 <chanidapa14870@gmail.com>
2018-03-13 00:43:52 +0700

    edit logout

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php   
     session_start(); //to ensure you are using same session
     session_destroy(); //destroy the session
-    header("location:/project1/index.php"); //to redirect back to "index.php" after logging out
+    echo '<script type="text/javascript"> 
+        window.location.href = "index.php";
+    </script>';
     exit();
 ?>
