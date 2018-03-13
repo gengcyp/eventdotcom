b1ba1e868f89637f9fcd22414cc15eea1d5287ca
5710404306 <chanidapa14870@gmail.com>
2018-03-13 23:01:38 +0700

    add send email

--- deleteData.php
+++ deleteData.php
@@ -8,6 +8,17 @@
 		
 	 	$result  = $db->delete('reservationtransaction',"WHERE reserveid = ".$_POST["entityId"]);
 	 	$result  = $db->delete('reservations',"WHERE reservationid = ".$_POST["entityId"]);	
-	 	
+
+	$userid = $db->select("userid",'reservations',"WHERE reservationid =".$_POST["entityId"]);
+    $email = $db->select("email",'users',"WHERE userid =".$userid[0][0]);
+    $eventcode = $db->select("eventcode","reservations","WHERE reservationid =".$_POST["entityId"];)
+    $eventname = $db->select("eventname","eventdetail","WHERE eventid =".$eventcode[0][0]);
+
+    $to      = $email[0][0];
+	$subject = 'Your Transaction of Event '.$eventname[0][0].' has been rejected';
+	$message = 'Your Transaction of Event '.$eventname[0][0].' has been rejected';
+	$headers = 'From: codehere@event.com';
+
+	mail($to, $subject, $message, $headers);
 	 }	
 ?>	
--- updateReservation.php
+++ updateReservation.php
@@ -7,5 +7,17 @@
 	 if ($_POST["path"] == 'update'){
 	
 	$result  = $db->update("reservations", "reservestatus = 1","WHERE reservationid =".$_POST["entityId"]);
+    $userid = $db->select("userid",'reservations',"WHERE reservationid =".$_POST["entityId"]);
+    $email = $db->select("email",'users',"WHERE userid =".$userid[0][0]);
+    $eventcode = $db->select("eventcode","reservations","WHERE reservationid =".$_POST["entityId"];)
+    $eventname = $db->select("eventname","eventdetail","WHERE eventid =".$eventcode[0][0]);
+
+    $to      = $email[0][0];
+	$subject = 'Your Transaction of Event '.$eventname[0][0].' has been Approved!!';
+	$message = 'Your Transaction of Event '.$eventname[0][0].' has been Approved!!';
+	$headers = 'From: codehere@event.com';
+
+	mail($to, $subject, $message, $headers);
+
 }
 ?>	
